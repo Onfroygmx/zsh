135030dd0fd9229e595cf70c15a25843ee61b3d8
OnfMBP <onfroy@gmx.com>
2021-07-27 18:45:43 +0200

    Add initial entries for zshenv variables

--- .dot/install.zsh
+++ .dot/install.zsh
@@ -1,1 +1,50 @@
+#!/usr/bin/env zsh
+#!/bin/zsh
+#   _____          _        _ _
+#  |_   _|        | |      | | |
+#    | | _ __  ___| |_ __ _| | |
+#    | || '_ \/ __| __/ _` | | |
+#   _| || | | \__ \ || (_| | | |
+#   \___/_| |_|___/\__\__,_|_|_|
 #
+# https://patorjk.com/software/taag/#p=display&c=bash&f=Doom&t=Install
+### Install basic tools
+
+## Autoload zsh functions
+#################################################
+autoload -U colors && colors
+
+## Export work folders
+#################################################
+export XDG_CONFIG_HOME=$HOME/.dot
+
+export ZDOTDIR=${XDG_CONFIG_HOME:-$HOME/.config}/zsh
+
+printf "\n$fg[green]Clone: Onfroygmx/zsh$reset_color\n"
+git clone --bare https://github.com/Onfroygmx/zsh.git $HOME/.dotgit
+git --git-dir=$HOME/.dotgit --work-tree=$HOME checkout
+
+printf "\n$fg[green]Symlink zshenv file$reset_color\n"
+## Set zshenv file
+[[ ! -f $HOME/.zshenv && -f $ZDOTDIR/zshenv ]] && ln -s $ZDOTDIR/zshenv $HOME/.zshenv
+
+PLUGINS_DIR="$XDG_CONFIG_HOME/Plugins"
+
+[[ ! -d "$PLUGINS_DIR" ]] && mkdir -p $PLUGINS_DIR
+
+find $XDG_CONFIG_HOME -type d -print0 | xargs -0 chmod 700
+mv .dotgit $XDG_CONFIG_HOME
+
+printf "\n$fg[Cyan]Clone external Plugins$reset_color\n"
+
+
+printf "\n$fg[green]Clone: zsh-users/zsh-autosuggestions$reset_color\n"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $PLUGINS_DIR/autosuggestions
+printf "\n$fg[green]Clone: zsh-users/zsh-completions$reset_color\n"
+git clone https://github.com/zsh-users/zsh-completions.git $PLUGINS_DIR/completions
+printf "\n$fg[green]Clone: zsh-users/zsh-history-substring-search$reset_color\n"
+git clone https://github.com/zsh-users/zsh-history-substring-search.git $PLUGINS_DIR/history-substring-search
+printf "\n$fg[green]Clone: zsh-users/zsh-syntax-highlighting$reset_color\n"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINS_DIR/syntax-highlighting
+
+printf "\n$fg[yellow]Install fininshed, restart ZSH$reset_color\n"
--- .dot/zsh/.zprofile
+++ .dot/zsh/.zprofile
@@ -1,1 +1,44 @@
+#!/usr/bin/env zsh
+#!/bin/zsh
+#   ______                 __ _ _
+#  |___  /                / _(_) |
+#     / / _ __  _ __ ___ | |_ _| | ___
+#    / / | '_ \| '__/ _ \|  _| | |/ _ \
+#  ./ /__| |_) | | | (_) | | | | |  __/
+#  \_____/ .__/|_|  \___/|_| |_|_|\___|
+#        | |
+#        |_|
 #
+## This file is sourced before zshrc
+
+################################
+# EXPORT ENVIRONMENT DIRECTORY #
+################################
+export MODULE_DIR=$ZDOTDIR/modules
+export PLUGIN_DIR=$XDG_CONFIG_HOME/plugins
+
+# Set specific XDG variables
+export XDG_DATA_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"/data
+
+# Set standard paths
+path=(
+  /usr/local/{sbin,bin}
+  /usr/{sbin,bin}
+  /{sbin,bin}
+  ~/bin
+  $path
+)
+
+# Set the list of directories that man searches for manuals.
+manpath=(
+  /usr/local/share/man
+  /usr/share/man
+  $manpath
+)
+
+# Ensure path arrays do not contain duplicates.
+typeset -gU path manpath fpath
+
+# Add completion add the begining of fpath and custom functions at the end
+fpath=( "$PLUGIN_DIR/completions/src" "${fpath[@]}" "$ZDOTDIR/functions" )
+autoload -Uz $fpath[-1]/*(.:t)

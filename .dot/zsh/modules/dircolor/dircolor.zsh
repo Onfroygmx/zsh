a64322b5fd63b447dda9f6937c9d7877f5336b7b
OnfMBP <onfroy@gmx.com>
2021-07-27 21:51:27 +0200

    Optimisation dircolors

--- .dot/zsh/modules/dircolor/dircolor.zsh
+++ .dot/zsh/modules/dircolor/dircolor.zsh
@@ -1,3 +1,10 @@
 #!/bin/zsh
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
-eval "$(dircolors -b ${0:a:h}/LS_COLORS)" # ls colors
+currentdir="${0:h}"
+# Source generated file only if does not exist or older then config file
+if [[ ! -f $currentdir/ls_colors.zsh || $currentdir/LS_COLORS -nt $currentdir/ls_colors.zsh ]]; then
+    dircolors -b $currentdir/LS_COLORS > $currentdir/ls_colors.zsh
+fi
+source $currentdir/ls_colors.zsh

2013ab5d960e6c14b757ebb89404fa64b813fb0a
OnfMBP <onfroy@gmx.com>
2021-07-28 11:24:28 +0200

    Don't load ls_colors twice

--- .dot/zsh/modules/dircolor/dircolor.zsh
+++ .dot/zsh/modules/dircolor/dircolor.zsh
@@ -6,5 +6,5 @@ currentdir="${0:h}"
 # Source generated file only if does not exist or older then config file
 if [[ ! -f $currentdir/ls_colors.zsh || $currentdir/LS_COLORS -nt $currentdir/ls_colors.zsh ]]; then
     dircolors -b $currentdir/LS_COLORS > $currentdir/ls_colors.zsh
+    source $currentdir/ls_colors.zsh
 fi
-source $currentdir/ls_colors.zsh

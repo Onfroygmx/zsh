6b939b3a17bbd4bf16dc9ef72ec2c6019d851071
Onf-MBP <onfroy@gmx.com>
2021-08-07 21:14:52 +0200

    Cleanup dircolor code

--- .dot/zsh/modules/dircolor/dircolor.zsh
+++ .dot/zsh/modules/dircolor/dircolor.zsh
@@ -2,9 +2,9 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-currentdir="${0:h}"
+local currentdir=${${(%):-%x}:a:h}
+
 # Source generated file only if does not exist or older then config file
 if [[ ! -f $currentdir/ls_colors.zsh || $currentdir/LS_COLORS -nt $currentdir/ls_colors.zsh ]]; then
     dircolors -b $currentdir/LS_COLORS > $currentdir/ls_colors.zsh
-#    source $currentdir/ls_colors.zsh
 fi
